b2847e07bc4f51d82bde68a5fb690f846346b762
Archie Mynott <70474549+archi-71@users.noreply.github.com>
2023-12-03 08:48:13 +0000

    Create Day3.cs

---
+++ 2023/Day3.cs
@@ -0,0 +1,130 @@
+using System.Collections.Generic;
+
+class Day3
+{
+    static void Main()
+    {
+        List<string> data = new List<string>();
+        StreamReader reader = new StreamReader("input.txt");
+        string? line = reader.ReadLine();
+        while (line != null)
+        {
+            data.Add(line);
+            line = reader.ReadLine();
+        }
+        reader.Close();
+
+        Console.WriteLine("Part 1 Answer: " + Part1(data));
+        Console.WriteLine("Part 2 Answer: " + Part2(data));
+    }
+
+    static int Part1(List<string> data)
+    {
+        int total = 0;
+        int currentNum = 0;
+        bool isPartNumber = false;
+        for (int i = 0; i < data.Count(); i++)
+        {
+            for (int j = 0; j < data[i].Length; j++)
+            {
+                if (char.IsNumber(data[i][j]))
+                {
+                    currentNum = currentNum * 10 + int.Parse(data[i][j].ToString());
+                    if (!isPartNumber)
+                    {
+                        for (int k = -1; k < 2; k++)
+                        {
+                            for (int l = -1; l < 2; l++)
+                            {
+                                if (i + k >= 0 && j + l >= 0 && i + k < data.Count() && j + l < data[i].Length)
+                                {
+                                    if (!(char.IsNumber(data[i + k][j + l]) || data[i + k][j + l] == '.'))
+                                    {
+                                        isPartNumber = true;
+                                        break;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    if (isPartNumber)
+                    {
+                        total += currentNum;
+                    }
+                    currentNum = 0;
+                    isPartNumber = false;
+                }
+            }
+        }
+        if (isPartNumber)
+        {
+            total += currentNum;
+        }
+        return total;
+    }
+
+    static int Part2(List<string> data)
+    {
+        Dictionary<string, List<int>> gears = new Dictionary<string, List<int>>();
+        int currentNum = 0;
+        string? gear = null;
+        for (int i = 0; i < data.Count(); i++)
+        {
+            for (int j = 0; j < data[i].Length; j++)
+            {
+                if (char.IsNumber(data[i][j]))
+                {
+                    currentNum = currentNum * 10 + int.Parse(data[i][j].ToString());
+                    if (gear == null)
+                    {
+                        for (int k = -1; k < 2; k++)
+                        {
+                            for (int l = -1; l < 2; l++)
+                            {
+                                if (i + k >= 0 && j + l >= 0 && i + k < data.Count() && j + l < data[i].Length)
+                                {
+                                    if (data[i + k][j + l] == '*')
+                                    {
+                                        gear = (i + k) + "-" + (j + l);
+                                        break;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    if (gear != null)
+                    {
+                        List<int>? parts;
+                        if (gears.TryGetValue(gear, out parts))
+                        {
+                            parts.Add(currentNum);
+                        }
+                        else
+                        {
+                            gears[gear] = new List<int> { currentNum };
+                        }
+
+                    }
+                    currentNum = 0;
+                    gear = null;
+                }
+            }
+        }
+        int total = 0;
+        List<string> gearList = new List<string>(gears.Keys);
+        foreach (string g in gearList)
+        {
+            if (gears[g].Count() == 2)
+            {
+                total += gears[g][0] * gears[g][1];
+            }
+        }
+        return total;
+    }
+}
